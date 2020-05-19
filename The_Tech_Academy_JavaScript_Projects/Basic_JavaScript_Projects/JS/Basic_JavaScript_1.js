8d2fba49e06157945e2547f6745374330de616fb
Courtney Dart <dart.courtney1@gmail.com>
2020-05-18 17:30:53 -0700

    Update file.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -1,4 +1,8 @@
-window.alert("Hello,world!");
-document.write('Hello, world!');
-var a = "Come say Hi!";
-document.write(a);
+// window.alert("Hello,world!");
+// document.write('Hello, world!');
+
+// var a = "Come say Hi!";
+// document.write(a);
+
+var x = "My name is:";
+window.alert(x);
