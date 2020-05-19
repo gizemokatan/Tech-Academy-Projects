64c94fcc8603155af27e4e534fdee5429d278597
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 11:17:24 -0700

    Add comments

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -1,12 +1,12 @@
 // pops up an alert window before viewing the page, 
-//  and prints the string in the body
+//  and prints the string in the body of the HTML
 
 // window.alert("Hello,world!");
 // document.write('Hello, world!');
 
 
 // assigns a string to a variable, 
-//  and writes the value of the string in the body.
+//  and writes the value of the string in the body of the HTML
 
 // var a = "Come say Hi!";
 // document.write(a);
@@ -17,19 +17,19 @@
 // var x = "My name is:";
 
 
-// must have double quotation marks ending this string.
+// must have double quotation marks ending this string if there is a quote or apostrohpy in the str
 
 // document.write("My question today is: \"How's your day going?\"");
 
 
-// a concatenated string.
+// concatenating a string
 
 // document.write("\"Be who you are and say what you feel,"
 // + " because those who mind don\'t matter and those who matter don\'t mind.\""
 // + "-Dr. Seuss");
 
 
-// Assign a concatenated string value to a variable.
+// Assign a concatenated string value to a variable
 
 // var b = ("\"A spoonful of sugar helps the medicine go down,"
 // + " the medicine go down... \""
@@ -37,13 +37,15 @@
 // document.write(b)
 
 
-// assign multiple variables in one statement
+// assign multiple variables in one statement and writes the selected variables value in the body of the HTML
 
 var Family = "The Smiths", Dad = "Tim", Mom = "Sue", Daughter = "Marian";
 document.write(Daughter);
 
 
-// We created a variable that was a string called “blues.” We assigned that variable the color blue
+// We created a variable that was a string called “green.” 
+// We assigned that variable the color green
+
 var green = "Tim";
 var green = green.fontcolor("green");
 document.write(green);
@@ -53,3 +55,8 @@ document.write(purple);
 var blue = "Marian";
 var blue = blue.fontcolor("blue");
 document.write(blue);
+
+
+// set the value of an expression to a variable and wrote the variables output in the body of the HTML
+y = 3 + 3;
+document.write(y);
