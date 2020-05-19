67117e17ed7cb17ece03cad058cd0928fef6598f
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 13:28:11 -0700

    Arithmetic Functions
    Add and subtract functions.

---
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,9 @@
+function addFunction () {  //Define funciton and name it
+    var addition = 2 + 2;  //Define variable to value of expression
+    document.getElementById("math").innerHTML = "2 + 2 = " + addition;  //Put the value of addition into the HTML element with 'math' id
+}
+
+function subFunction () {
+    var subtraction = 5 - 2;
+    document.getElementById("math2").innerHTML = "5 - 2 = " + subtraction;
+}
