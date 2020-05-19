d8ce1dfdbe9d994480200f960a539a08ee7b0a30
Courtney Dart <dart.courtney1@gmail.com>
2020-05-18 18:18:57 -0700

    Assign multiple variables and font color of variables.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -1,20 +1,55 @@
-// window.alert("Hello,world!");
-// document.write('Hello, world!');
 // pops up an alert window before viewing the page, 
 //  and prints the string in the body
 
-// var a = "Come say Hi!";
-// document.write(a);
+// window.alert("Hello,world!");
+// document.write('Hello, world!');
+
+
 // assigns a string to a variable, 
 //  and writes the value of the string in the body.
 
-// var x = "My name is:";
-// window.alert(x);
+// var a = "Come say Hi!";
+// document.write(a);
 
-// document.write("My question today is: \"How's your day going?\"");
+
+
+// window.alert(x);
+// var x = "My name is:";
+
+
 // must have double quotation marks ending this string.
 
-document.write("\"Be who you are and say what you feel,"
-+ " because those who mind don\'t matter and those who matter don\'t mind.\""
-+ "-Dr. Seuss");
+// document.write("My question today is: \"How's your day going?\"");
+
+
 // a concatenated string.
+
+// document.write("\"Be who you are and say what you feel,"
+// + " because those who mind don\'t matter and those who matter don\'t mind.\""
+// + "-Dr. Seuss");
+
+
+// Assign a concatenated string value to a variable.
+
+// var b = ("\"A spoonful of sugar helps the medicine go down,"
+// + " the medicine go down... \""
+// + "- Mary Poppins");
+// document.write(b)
+
+
+// assign multiple variables in one statement
+
+var Family = "The Smiths", Dad = "Tim", Mom = "Sue", Daughter = "Marian";
+document.write(Daughter);
+
+
+// We created a variable that was a string called “blues.” We assigned that variable the color blue
+var green = "Tim";
+var green = green.fontcolor("green");
+document.write(green);
+var purple = "Sue";
+var purple = purple.fontcolor("purple");
+document.write(purple);
+var blue = "Marian";
+var blue = blue.fontcolor("blue");
+document.write(blue);
