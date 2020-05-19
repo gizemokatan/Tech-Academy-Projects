97374a4916147ea0a143307238bef6fdc3d038b1
Courtney Dart <dart.courtney1@gmail.com>
2020-05-18 17:54:03 -0700

    Update file with quote and concatenation.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -1,8 +1,20 @@
 // window.alert("Hello,world!");
 // document.write('Hello, world!');
+// pops up an alert window before viewing the page, 
+//  and prints the string in the body
 
 // var a = "Come say Hi!";
 // document.write(a);
+// assigns a string to a variable, 
+//  and writes the value of the string in the body.
 
-var x = "My name is:";
-window.alert(x);
+// var x = "My name is:";
+// window.alert(x);
+
+// document.write("My question today is: \"How's your day going?\"");
+// must have double quotation marks ending this string.
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+// a concatenated string.
