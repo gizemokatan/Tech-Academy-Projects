2cb06afb0b8fc64e99079953d0e05178febc701b
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 12:11:55 -0700

    Utilize JS function

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
@@ -1,5 +1,5 @@
 function myFunction() {  //Define function and name it
-    var str = "My favorite color is green";  //Define variable and gave it a string value
+    var str = "My favorite color is green!";  //Define variable and gave it a string value
     var result =  str.fontcolor("green");  //Use fontcolor method on the str variable
     document.getElementById("green_text").innerHTML = result; //Put the value of result into the HTML element with 'green_text' id
 }
