3dce11ac70571770a8c7d5ed4e670d44f4c26d33
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 13:40:34 -0700

    Arithmetic functions to multiply and divide.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
@@ -5,5 +5,15 @@ function addFunction () {  //Define funciton and name it
 
 function subFunction () {
     var subtraction = 5 - 2;
-    document.getElementById("math2").innerHTML = "5 - 2 = " + subtraction;
+    document.getElementById("math2").innerHTML = "5 - 2 = " + subtraction; //give it a new id so it doesnt display the the result in the 'Add me' button when clicking the 'Subtract me' button
+}
+
+function multiplication () {
+    var multiply = 6 * 8;
+    document.getElementById("math3").innerHTML = "6 * 8 = " + multiply; // id 'math3' is yet another new id
+}
+
+function division () {
+    var divide = 48 / 6;
+    document.getElementById("math4").innerHTML = "48 / 6 = " + divide;
 }
