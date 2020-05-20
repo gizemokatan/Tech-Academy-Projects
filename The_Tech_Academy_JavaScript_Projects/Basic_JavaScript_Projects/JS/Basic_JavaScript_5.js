0ab779f6254a254f51c0005818dbcc7483953f40
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 19:33:38 -0700

    Use Boolean logic.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -1,4 +1,4 @@
-document.writeln(typeof 3)
+document.writeln(typeof 3)  //gives the type of the value
 document.writeln(typeof "song")
 
 function myFunction() {
@@ -13,5 +13,18 @@ function myFunction2() {
     document.getElementById("test2").innerHTML = isNaN(007);
 }
 
-document.writeln(2E310);
-document.writeln(-3E310);
+function infin() {  //define a function for an output of infinity
+    document.writeln(2E310);
+}
+
+function negInfin() {  //define a function for an output of -infinity
+    document.writeln(-3E310); 
+}
+
+function Bool() {  //using Boolean logic to result True
+    document.writeln(10 > 2);
+}
+
+function Bool1() {  //using Boolean logic to result False
+    document.writeln(10 < 2);
+}
