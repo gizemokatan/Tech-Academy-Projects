2d29043175599012a500beec07d150ba99db7605
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 18:13:35 -0700

    Try to use same name kvp example.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_4.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_4.js
@@ -1,9 +1,11 @@
-function my_dictionary() {
-    var student = {
+function my_dictionary() {  //defin and name a funtion for a dictionary
+    var student = {        // define a variable given the following key value pairs
         firstName: "Nicson",
         lastName: "Martinez",
         age: 27,
-        averageGPA: 4.0
+        averageGPA: 4.0,
+        averageGPA: 3.5 //creating another key with the same name will skew results
+                        // calling student.averageGPA will only result in the second KVP value of 3.5
     };
-    document.getElementById("dictionary").innerHTML = student.firstName;
+    document.getElementById("dictionary").innerHTML = student.averageGPA;
 }
