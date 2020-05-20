7a80a1b4a613722b83898b832ea7be04da8a3da2
Courtney Dart <dart.courtney1@gmail.com>
2020-05-20 13:16:03 -0700

    Keywords and constructor example
    Utilize 'new' and 'this' keywords.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_6.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_6.js
@@ -15,3 +15,28 @@ function vote_function() { //Define and name a function
                                                                                 //States if it's true that the user input is less than 18 then True, but if user input is 18 or older then False
     document.getElementById("result").innerHTML = can_vote;  //Displays the result on the HTML element with 'result' id
 }
+
+document.write("<br>")
+
+function vehicle(make, model, year, color) {  //Define and name a class with four parameters
+    this.vehicle_make = make;  //allows for possession to take place when we call a specified variable
+    this.vehicle_model = model;
+    this.vehicle_year = year;
+    this.vehicle_color = color;
+}
+var Jack = new vehicle("Dodge", "Viper", 2020, "red"); //Define variable and create a new instance of our vehicle class
+var Erik = new vehicle("Ford", "Pinto", 1971, "mustard");
+function myFunction() {  //Define and name a function
+    document.getElementById("keywords_and_constructors").innerHTML =   //Display the output of a specified instance
+    "Erik drives a " + Erik.vehicle_color + "-colored " + Erik.vehicle_model +
+    " manufactured in " + Erik.vehicle_year;
+}
+
+//Assignment Example
+
+var Courtney = new vehicle("Hyundai", "Sonata", 2007, "navy blue");
+function myFunction1() {
+    document.getElementById("new_and_this").innerHTML = 
+    "Courtney drives a " + Courtney.vehicle_color + " " + Courtney.vehicle_year
+    + " " + Courtney.vehicle_make + " " + Courtney.vehicle_model;
+}
