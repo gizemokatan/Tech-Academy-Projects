2aefdc9d7470d18afac61c1b6e1a9f2eb25a9734
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 18:21:13 -0700

    Use delete operator in dictionary.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_4.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_4.js
@@ -9,3 +9,15 @@ function my_dictionary() {  //defin and name a funtion for a dictionary
     };
     document.getElementById("dictionary").innerHTML = student.averageGPA;
 }
+
+function new_dictionary() {
+    var animal = {
+        species: "dog",
+        color: "brown",
+        breed: "goldendoodle",
+        age: 3,
+        sound: "bark"
+    }
+    delete animal.sound;  //this line is what makes the output display 'undefinded' since the value for sound no longer exists in the dictionary
+    document.getElementById("animal").innerHTML = animal.sound;
+}
