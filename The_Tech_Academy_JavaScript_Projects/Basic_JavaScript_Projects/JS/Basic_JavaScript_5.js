2df032b8f27cfe56e24babae0dacecbf2072d180
Courtney Dart <dart.courtney1@gmail.com>
2020-05-19 21:15:47 -0700

    Utilize double equals and triple equals.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -40,3 +40,37 @@ function newType() {  //converts the value to the type coercion it chooses
 function BoolCon() {
     console.log(2 > 3);
 }
+
+function dbequals() { // gives a Boolean logic result of true
+    document.writeln(10==10);
+}
+
+function dbequals1() {  //// gives a Boolean logic result of false
+    document.writeln(2==10);
+}
+
+function Age() {  //Return true by ensuring to match the data type and value
+    var age = 26;
+    var age1 = 26;
+    document.writeln(age === age1);
+}
+
+function Age1() { //Return false by writing a different data type and different value
+    var age = 26;
+    var dateofbirth = 10/09/1980;
+    document.writeln(age === dateofbirth);
+}
+
+function Age2() { //Return false by writing a different data type but the same value for both
+    var age = 26;
+    var age1 = '26';
+    document.writeln(age === age1);
+}
+
+function Age3() {  //Return false by writing the same data type but a different value for both
+    var age = 26;
+    var age1 = 28;
+    document.writeln(age === age1);
+}
+
+
