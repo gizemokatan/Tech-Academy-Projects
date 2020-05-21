4a8c4037515d8a5c5d85f88c471e6831b1118d1a
Courtney Dart <dart.courtney1@gmail.com>
2020-05-21 12:23:19 -0700

    Convert string to upper case and search for text position in a string.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_8.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_8.js
@@ -16,5 +16,25 @@ function sliceMethod() {
     var text = "I would've gotten away with it if it hadn't been for those meddling kids";
     // document.getElementById("slice").innerHTML = text.length; //Provides the length of the string
     var section = text.slice(53, 72);  //Define and name another variabel to store the result from the slice method
-    document.getElementById("slice1").innerHTML = section; //Display the section that was extracted from the original string in the HTML element with 'slice' id
+    document.getElementById("slice").innerHTML = section; //Display the section that was extracted from the original string in the HTML element with 'slice' id
+}
+
+document.write("<br><br>")
+
+//toUpperCase Method Ex
+
+function upperFunction() {
+    var text_now = "Hello world!";
+    document.getElementById("upper_case").innerHTML = text_now.toUpperCase(); //always put parenthesis after a method otherwise 
+                                                                            //its unable to execute
+}
+
+document.write("<br><br>")
+
+//search() method ex
+
+function searchFunction() {
+    var strings = "Please find the first time 'find' is stated";
+    var posi = strings.search("find"); //using the seacrh() method it locates the first occurance of the specified text in the string
+    document.getElementById("search").innerHTML = posi; //Displays the number associated with the first character's position 'f' in the text
 }
