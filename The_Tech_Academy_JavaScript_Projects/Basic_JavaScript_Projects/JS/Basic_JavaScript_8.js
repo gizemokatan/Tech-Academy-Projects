c9f049378d5949c02e0a43586b3ab6326ccc1f6a
Courtney Dart <dart.courtney1@gmail.com>
2020-05-21 11:42:38 -0700

    Utilize slice method.

--- The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_8.js
+++ The_Tech_Academy_JavaScript_Projects/Basic_JavaScript_Projects/JS/Basic_JavaScript_8.js
@@ -9,3 +9,12 @@ function fullSentence() {
 }
 
 document.write("<br><br>")
+
+//Slice() Method Ex
+
+function sliceMethod() {
+    var text = "I would've gotten away with it if it hadn't been for those meddling kids";
+    // document.getElementById("slice").innerHTML = text.length; //Provides the length of the string
+    var section = text.slice(53, 72);  //Define and name another variabel to store the result from the slice method
+    document.getElementById("slice1").innerHTML = section; //Display the section that was extracted from the original string in the HTML element with 'slice' id
+}
