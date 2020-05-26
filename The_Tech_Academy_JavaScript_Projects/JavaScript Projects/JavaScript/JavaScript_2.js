a748a0d377cd67840c2332e13b169b5a7cc7e9fc
Courtney Dart <dart.courtney1@gmail.com>
2020-05-26 15:04:26 -0700

    Form challenge
    Utilize JS to validate form, and css to style form.

---
+++ The_Tech_Academy_JavaScript_Projects/JavaScript Projects/JavaScript/JavaScript_2.js
@@ -0,0 +1,44 @@
+// function validateForm() {
+// //The second bracket is referencing the name attribute inside myForm
+//     var fname = document.forms["myForm"]["fname"].value;
+//     var lname = document.forms["myForm"]["lname"].value;
+//     var email = document.forms["myForm"]["email"].value;
+//     var phone = document.forms["myForm"]["phone"].value;
+//     if (1 === 1) {
+//         alert(fname);
+//         return false;
+//     }
+//     if (lname === "") {
+//         alert("Last Name must be filled out");
+//         return false;
+//     }
+//     if (email === "") {
+//         alert("Email Address must be filled out");
+//         return false;
+//     }
+//     if (phone === "") {
+//         alert("Phone Number must be filled out");
+//         return false;
+//     }
+// }
+
+var list_of_empty_fields = [];
+function validateForm() {
+    var field = document.getElementsByClassName("field");
+    for (var i = 0; i < field.length; i++) {
+        if (field[i].value === "") {
+            list_of_empty_fields.push(field[i].id);
+        }
+    }
+
+    if (list_of_empty_fields.length === 0) {
+        alert("Your form was submitted successfully")
+    }
+    else {
+        var empty_fields = list_of_empty_fields.toString();
+        alert(empty_fields + " must be filled out")
+    }
+    console.log(list_of_empty_fields);
+}
+
+
