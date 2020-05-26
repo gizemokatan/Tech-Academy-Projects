e772355b0f6d2d73e2e3a9ab176df872406f6805
Courtney Dart <dart.courtney1@gmail.com>
2020-05-26 15:03:22 -0700

    Add graphics with canvas tag

--- The_Tech_Academy_JavaScript_Projects/JavaScript Projects/JavaScript/JavaScript_1.js
+++ The_Tech_Academy_JavaScript_Projects/JavaScript Projects/JavaScript/JavaScript_1.js
@@ -39,3 +39,44 @@ function HWfunction() {
     var a = document.getElementsByClassName("click");
     a[0].innerHTML = "It's not magic!";
 }
+
+//drawing graphics with canvas tag
+
+var c = document.getElementById("myCanvas");
+
+/* The getContext() method returns an object that provides 
+methods and properties for drawing on the canvas.
+Its simpler to define it in a variable. */
+var ctx = c.getContext("2d");
+
+ctx.font = "20px Georgia";
+
+//parameters for fillText are (text, x, y)
+ctx.fillText("Hello World!", 10, 50);
+
+ctx.font = "30px Veranda";
+//Create gradient
+var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+
+/*parameters for addColorStop are (stop, color), where stop is
+a value between 0.0 and 1.0 that represents the position between
+start and end in a gradient.*/
+gradient.addColorStop("0", "magenta");
+gradient.addColorStop("0.5", "blue");
+gradient.addColorStop("1.0", "green");
+//Fill with gradient
+ctx.fillStyle = gradient;
+ctx.fillText("I made this with JavaScript!", 10, 90);
+
+// //create background gradient left to right
+// var bgGradient = ctx.createLinearGradient(0, 0, 170, 0);
+// bgGradient.addColorStop(0, "white");
+// bgGradient.addColorStop(0.5, "#B0C4DE");
+// bgGradient.addColorStop(1, "#4682B4")
+
+// ctx.fillStyle = bgGradient;
+
+// /* parameters reflect the full width and height of rectangle 
+// and will fill the entire rectangle */
+// ctx.fillRect(0, 0, 500, 250);
+
