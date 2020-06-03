8f2004b209c9c3c04b2614164c6b455d036c7835
Courtney Dart <dart.courtney1@gmail.com>
2020-06-02 18:23:34 -0700

    Add popup form

--- The_Tech_Academy_JavaScript_Projects/JavaScript Projects/JavaScript/animalSpecies.js
+++ The_Tech_Academy_JavaScript_Projects/JavaScript Projects/JavaScript/animalSpecies.js
@@ -2,3 +2,12 @@ function displayType(animal) {
     var animalSpecies = animal.getAttribute("data-animal-type");
     alert(animalSpecies + " is in the " + animal.innerHTML + " class.");
 }
+
+// Functions to open and close the popup form.
+function openForm() {
+    document.getElementById("animalForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("animalForm").style.display = "none";
+}
