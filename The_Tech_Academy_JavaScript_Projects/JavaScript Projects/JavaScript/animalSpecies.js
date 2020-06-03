821845acba1f42d304fe5a5e9f4bad0c153938e1
Courtney Dart <dart.courtney1@gmail.com>
2020-06-03 11:53:09 -0700

    Create automatic slideshow of images

--- The_Tech_Academy_JavaScript_Projects/JavaScript Projects/JavaScript/animalSpecies.js
+++ The_Tech_Academy_JavaScript_Projects/JavaScript Projects/JavaScript/animalSpecies.js
@@ -11,3 +11,15 @@ function openForm() {
 function closeForm() {
     document.getElementById("animalForm").style.display = "none";
 }
+
+// Added jQuery to 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
