28ded4e6864bbc3cef7288d07f6c761276f1c552
Courtney Dart <dart.courtney1@gmail.com>
2020-06-03 14:42:13 -0700

    Create a sticky button popup form

---
+++ The_Tech_Academy_HTML_and_CSS_Projects/HTML Documents/js/assignmentJS.js
@@ -0,0 +1,8 @@
+// Functions to open and close the popup form.
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+}
