ad2805e3edf8978c98d7bc7fc303e8e246a63383
Courtney Dart <dart.courtney1@gmail.com>
2020-06-05 06:35:45 -0700

    Combine two slide shows- attempt 2.

--- The_Tech_Academy_HTML_and_CSS_Projects/Portfolio/Courtney's_Portfolio/js/Courtney's_portfolio.js
+++ The_Tech_Academy_HTML_and_CSS_Projects/Portfolio/Courtney's_Portfolio/js/Courtney's_portfolio.js
@@ -8,14 +8,38 @@ function openForm() {
   }
 
 // Added jQuery to slideshow
-$("#slideshow2 > div:gt(0)").hide();
+$("#Slideshow_Container > div:gt(0)").hide();
 
 setInterval(function() {
-    $('#slideshow2 > div:first')
+    $('#Slideshow_Container > div:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
-    .appendTo('#slideshow2');
+    .appendTo('#Slideshow_Container');
 }, 3000);
+
+// $(document).ready(function(){   
+//   $("a").on('click', function(event) {
+	  
+//     // Make sure this.Link has a value before overriding default behavior
+//     if (this.Link !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+
+//       // Store Link
+//       var Link = this.Link;
+
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (2000) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(Link).offset().top
+//       }, 2000, function(){
+   
+//         // Add Link (#) to URL when done scrolling (default click behavior)
+//         window.location.Link = Link;
+//       });
+//     } // End if
+//   });
+// });
   
