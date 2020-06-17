fdd44cbe24d5edbaa6eaa8474028ad7f634a411e
Courtney Dart <dart.courtney1@gmail.com>
2020-06-17 15:17:15 -0700

    Add Accounting Assignment

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/AccountingAssignment/AccountingAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AccountingAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting System");
+            //scape character 
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
