9bfa64bd2f7b562293ae7e853f479750b9e478de
Courtney Dart <dart.courtney1@gmail.com>
2020-06-16 18:46:29 -0700

    First Assignment VS

---
+++ The_Tech_Academy_Visual_Studio_Projects/BasicHTMLVideoWebPage/BasicHTMLVideoWebPage/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var i = 0;
+var txt = '1. Life Lesson to learn: Be Humble!';
+var speed = 100;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("typing").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
