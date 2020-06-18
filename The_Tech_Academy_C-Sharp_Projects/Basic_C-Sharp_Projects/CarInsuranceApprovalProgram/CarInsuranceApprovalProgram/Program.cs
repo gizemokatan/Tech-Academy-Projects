945721e8c65012a3e509e0728755bd34a14c722d
Courtney Dart <dart.courtney1@gmail.com>
2020-06-18 12:19:11 -0700

    Create Approval Program for Car Insurance Console App

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/CarInsuranceApprovalProgram/CarInsuranceApprovalProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CarInsuranceApprovalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? (Please enter a number):");
+            string UserAge = Console.ReadLine();
+            int UserAgeNum = Convert.ToInt32(UserAge);
+
+            Console.WriteLine("Have you ever had a DUI? (Please enter 'true' for yes or 'false' for no):");
+            string HadDUI = Console.ReadLine();
+            bool HadDUITrue = Convert.ToBoolean(HadDUI);
+
+            Console.WriteLine("How many speeding tickets have you had? If none, enter 0. (Please enter the number of speeding tickets as a number):");
+            string HadSpeedingTicket = Console.ReadLine();
+            int HadSpeedingTicketTrue = Convert.ToInt32(HadSpeedingTicket);
+
+            Console.WriteLine("Applicant qualifies for car insurance?: ");
+            bool Qualifies = UserAgeNum > 15 && HadDUITrue == false && HadSpeedingTicketTrue <= 3;
+            Console.WriteLine(Qualifies);
+            Console.ReadLine();
+
+        }
+    }
+}
