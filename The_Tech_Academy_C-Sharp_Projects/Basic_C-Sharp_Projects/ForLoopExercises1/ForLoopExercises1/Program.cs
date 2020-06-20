d9e0971bcd4b19ab35a192ec1dde238dd1f07e0e
Courtney Dart <dart.courtney1@gmail.com>
2020-06-19 21:07:13 -0700

    Create an array of strings, update a string  based on user input., utilize foreach and for loops.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ForLoopExercises1/ForLoopExercises1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Let us know your favorite ice cream flavor from this list.\n");
+        string[] iceCreamFlavors = { "Chocolate", "Vanilla", "Strawberry", "Mint Chip", "Chunky Monkey" };
+
+        foreach (string flavor in iceCreamFlavors)
+        {
+            Console.WriteLine(flavor);
+        }
+       
+        Console.WriteLine("\nPlease type one of the flavors from the list above:\n");
+  
+        string userFlavor = Console.ReadLine();
+        Console.WriteLine();
+
+        for (int i = 0; i < iceCreamFlavors.Length; i++)
+        {
+            //if the flavor equalls what the user input, then concatenate at that index
+            if (userFlavor == iceCreamFlavors[i]) 
+            {
+                iceCreamFlavors[i] = iceCreamFlavors[i] + " is your favorite flavor!";
+            }
+        }
+
+        foreach (string flavor in iceCreamFlavors)
+        {
+            Console.WriteLine(flavor);
+        }
+        Console.ReadLine();
+
+    }
+    }
