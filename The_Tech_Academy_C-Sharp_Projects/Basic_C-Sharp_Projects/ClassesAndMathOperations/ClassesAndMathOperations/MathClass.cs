fb5bf3f031bd36126e364bddcb4b66fac55e3091
Courtney Dart <dart.courtney1@gmail.com>
2020-06-23 15:11:57 -0700

    Create a class and methods, utilize methods to perform math operations.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOperations/ClassesAndMathOperations/MathClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOperations
+{
+    public class MathClass
+    {
+        // These are methods of the class, and the class is a model to look at. This doesn't create any objects.
+        // These are for when I create an object, to know what it will have.
+        public static int addNumbers(int num)
+        {
+            num = num + 10;
+            return num;
+        }
+        
+        public int subtractNumbers(int num)
+        {
+            num = num - 10;
+            return num;
+        }
+
+        public int multiplyNumbers(int num)
+        {
+            num = num * 10;
+            return num;
+        }
+
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOperations/ClassesAndMathOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do some basic Math...\n");
+            Console.WriteLine("Please enter a number:\n");
+            Console.WriteLine("1. Your number will be added to 10");
+            Console.WriteLine("1. Your number will be subtracted by 10");
+            Console.WriteLine("1. Your number will be multiplied by 10\n");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // subtractNumbers is not static, therefore we can create multiple instanciations of MathClass class,
+            // whereas addNumbers is static and an object doesn't need to be instanciated for it to be used from MathClass.
+            MathClass math101 = new MathClass();
+
+            int addNumbersResult = MathClass.addNumbers(userNumber);
+            int subtractNumbersResult = math101.subtractNumbers(userNumber);
+            int multiplyNumbersResult = math101.subtractNumbers(userNumber);
+
+            Console.WriteLine(userNumber + " + 10 = " + addNumbersResult);
+            Console.WriteLine(userNumber + " - 10 = " + subtractNumbersResult);
+            Console.WriteLine(userNumber + " * 10 = " + multiplyNumbersResult);
+            Console.ReadLine();
+
+
+        }
+    }
+}
