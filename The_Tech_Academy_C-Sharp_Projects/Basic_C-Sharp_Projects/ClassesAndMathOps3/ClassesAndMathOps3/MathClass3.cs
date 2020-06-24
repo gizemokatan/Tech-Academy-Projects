b55d4903dffd621ff40d960d996c7385ab320399
Courtney Dart <dart.courtney1@gmail.com>
2020-06-23 21:26:43 -0700

    Create a void class, instanciate object math104 of class MathClass3, and call method.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOps3/ClassesAndMathOps3/MathClass3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOps3
+{
+    class MathClass3
+    {
+        public void divideNumbers(int num, int num1)
+        {
+            int numsResult = num / 2;
+            Console.WriteLine("This is your second number: {0} ", num1);
+        }
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOps3/ClassesAndMathOps3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOps3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass3 math104 = new MathClass3();
+
+            int valueOne = 10;
+            int ValueTwo = 21;
+
+            // valueOne will be divided by 2 behind the scenes, and valueTwo will be displayed on the console.
+            math104.divideNumbers(num: valueOne, num1: ValueTwo);
+
+            Console.WriteLine("The End.");
+            Console.ReadLine();
+        }
+    }
+}
