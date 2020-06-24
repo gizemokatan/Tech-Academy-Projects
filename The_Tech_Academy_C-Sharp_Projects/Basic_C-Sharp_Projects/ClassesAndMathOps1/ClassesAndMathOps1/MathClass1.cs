d413099ffd593d0e853898c55fbada77ffad0ccf
Courtney Dart <dart.courtney1@gmail.com>
2020-06-23 18:08:56 -0700

    Create a class and methods, instanciated a MathClass object, and perform addition operations.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOps1/ClassesAndMathOps1/MathClass1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOps1
+{
+    class MathClass1
+    {
+        public int addNumber(int num)
+        {
+            num = num + 25;
+            return num;
+        }
+
+        public int addNumber(decimal num)
+        {
+            num = num + 50.5m;
+            int decimalNum = Convert.ToInt32(num); 
+            return decimalNum;
+        }
+
+        public int addNumber(string num) 
+        {
+            return Convert.ToInt32(num) + 5;
+        }
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOps1/ClassesAndMathOps1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOps1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A simple math operation...");
+
+            MathClass1 math102 = new MathClass1();
+
+            int result = math102.addNumber(12);
+            int result1 = math102.addNumber(12.99m);
+            int result2 = math102.addNumber("26");
+
+            Console.WriteLine("Here is the result of adding 25 to the number 12:");
+            Console.WriteLine(result);
+            Console.WriteLine("Here is the result of adding 50.5 to the decimal number 12.99 and returning it as an integer:");
+            Console.WriteLine(result1);
+            Console.WriteLine("Here is the age 26 in 5 years from now:");
+            Console.WriteLine(result2 + " ... Time flies ...");
+            Console.ReadLine();
+        }
+        
+    }
+}
