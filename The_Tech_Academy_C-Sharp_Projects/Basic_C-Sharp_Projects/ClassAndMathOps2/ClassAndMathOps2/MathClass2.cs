397a4341c30d4dd4c49314379018bee974d0b072
Courtney Dart <dart.courtney1@gmail.com>
2020-06-23 20:42:14 -0700

    Create a class and method,  utilize if statement for a math operation of either one or two passed in parameters.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassAndMathOps2/ClassAndMathOps2/MathClass2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMathOps2
+{
+    class MathClass2
+    {
+        public int multiplyNumbers(int num, int num1 = 1)
+        {
+            return num * num1;
+        }
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassAndMathOps2/ClassAndMathOps2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMathOps2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass2 math103 = new MathClass2();
+
+            int result;
+
+            Console.WriteLine("Please enter a number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number, but please note, you don't have to enter anything:");
+            string secondEntered = Console.ReadLine();
+            if (string.IsNullOrEmpty(secondEntered))
+            {
+                result = math103.multiplyNumbers(userNum);
+            }
+            else
+            {
+                result = math103.multiplyNumbers(userNum, Convert.ToInt32(secondEntered));
+            }
+     
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
