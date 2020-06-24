2025513fec3c0d60ac566aee4758abc305888884
Courtney Dart <dart.courtney1@gmail.com>
2020-06-24 10:25:31 -0700

    Create a class, create a void method and method overload with out parameter to perform a basic math operation.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOps4/ClassesAndMathOps4/MathClass4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOps4
+{
+    // Create a class
+    class MathClass4
+    {
+        // Create a void method that outputs an integer and divides the data passed to it by 2
+        public void DivideByTwo(int num)
+        {
+            int numResult = num / 2;
+            Console.WriteLine(numResult);
+
+        }
+
+        //  Create a method with output parameters and overload a method
+        public void DivideByTwo(int num, out int numResult)
+        {
+            numResult = num / 2;
+        }
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ClassesAndMathOps4/ClassesAndMathOps4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMathOps4
+{
+    // Declare a class to be static
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathClass4 math105 = new MathClass4();
+
+            try
+            {
+                Console.WriteLine("Please enter a number:");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int numResult;
+
+                // Call the method on the userInput
+                math105.DivideByTwo(userInput, out numResult);
+
+                Console.WriteLine("The result of {0} divided by 2 = {1}", userInput, numResult);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("ERROR: Please enter a whole number.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
