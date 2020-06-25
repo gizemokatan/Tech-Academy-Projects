8de519498d8dc5a7aa596301fc870e1e44c0e5f7
Courtney Dart <dart.courtney1@gmail.com>
2020-06-25 15:23:29 -0700

    Create an overload operator and check if two Employee objects  are equal by comparing Id properties.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    public class Employee
+    {
+        // Create a property called Id
+        public int Id { get; set; }
+
+        public static Employee operator== (Employee employee, Employee Id)
+        {
+            employee.Equals(Id);
+            return employee;
+        }
+        // operator ' == ' required there be the ' != ' operator also defined
+        public static Employee operator!= (Employee employee, Employee Id)
+        {
+            employee.Equals(Id);
+            return employee;
+        }
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate two employee objects 
+            Employee john = new Employee();
+            Employee cathy = new Employee();
+            john.Id = 222;
+            cathy.Id = 333;
+
+            // Compare Id properties of employee objects 
+            Console.WriteLine("Do employees, John and Cathy, have the same Id...?");
+            bool employeesEqual = john.Id == cathy.Id;
+
+            Console.WriteLine(employeesEqual);
+            Console.ReadLine();
+        }
+    }
+}
