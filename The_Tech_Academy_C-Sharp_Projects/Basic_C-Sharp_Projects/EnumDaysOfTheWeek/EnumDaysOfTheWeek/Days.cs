b12f5b554b6609ac6b5a4007448903b0862b738d
Courtney Dart <dart.courtney1@gmail.com>
2020-07-03 08:38:29 -0700

    Create and enum for days of the week, utilize the parse method inside the enum class, create a method to uppercase, and utilize a try/catch.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/EnumDaysOfTheWeek/EnumDaysOfTheWeek/Days.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDaysOfTheWeek
+{
+    // Create an enum for the days of the week
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/EnumDaysOfTheWeek/EnumDaysOfTheWeek/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDaysOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueProgram = true;
+            while (continueProgram)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the day of the week:");
+                    string weekday = FirstCharToUpper(Console.ReadLine());
+
+                    // Assign the value to a variable of that enum data type
+                    object result = Enum.Parse(typeof(DaysOfTheWeek), weekday);
+
+                    Console.WriteLine(result);
+                    continueProgram = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong. Please enter an actual day of the week.");
+                }
+            }
+            Console.ReadLine();
+
+        }
+        // The method takes in a string parameter 
+        // This method is static and therefore the class doesnt need to be instanciated
+        public static string FirstCharToUpper(string s)
+        {
+            // Check for empty string.  
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.  
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+}
