b5db6709f879e2c1ffc1694735fd6f10f2fc4597
Courtney Dart <dart.courtney1@gmail.com>
2020-07-03 12:37:18 -0700

    Create a struct and instantiate an object.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/Structs/Structs/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    // Create a struct with the property Amount of data type decimal
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/Structs/Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 19.3m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
