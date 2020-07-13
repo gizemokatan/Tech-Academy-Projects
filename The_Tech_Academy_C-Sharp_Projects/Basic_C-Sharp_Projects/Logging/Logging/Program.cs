096665a708cb429b4ee17385b1e5fd668cd6b7d1
Courtney Dart <dart.courtney1@gmail.com>
2020-07-13 14:29:56 -0700

    Log a number to a text file.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/Logging/Logging/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello there! Please enter a number: ");
+            string userNumber = Console.ReadLine();
+            // Log that number to a text file.
+            File.WriteAllText(@"C:\Users\marti\Logs\log1.txt", userNumber);
+
+            //Print the text file back to the user.
+            string userNumber1 = File.ReadAllText(@"C:\Users\marti\Logs\log1.txt");
+
+            Console.WriteLine("Here is whats in the text file: ");
+            Console.WriteLine(userNumber1);
+            Console.ReadLine();
+        }
+        
+    }
+}
