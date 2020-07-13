4a25f7def5e07827ad166ce9102ab10836113b1d
Courtney Dart <dart.courtney1@gmail.com>
2020-07-13 16:10:19 -0700

    Utilize DateTime and TimeStamp for Logging.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/DateTime/DateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("The current date and time is:");
+        DateTime currentDateTime = DateTime.Now;
+        Console.WriteLine(currentDateTime);
+
+        Console.WriteLine("Please enter a number:\nWe will then check the time it will be 'x' number of hours from now. ");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+        TimeSpan numberHours = new TimeSpan(userNumber, 0, 0);
+
+        DateTime timeInXHours = currentDateTime + numberHours;
+        Console.WriteLine(timeInXHours);
+        Console.ReadLine();
+    }
+}
