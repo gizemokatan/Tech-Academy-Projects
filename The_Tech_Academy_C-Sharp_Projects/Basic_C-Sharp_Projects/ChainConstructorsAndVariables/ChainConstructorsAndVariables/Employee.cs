d1a77c64513ab03e27e30cefdd7b9a04fa44a261
Courtney Dart <dart.courtney1@gmail.com>
2020-07-14 15:07:28 -0700

    Utilize constructor call chain.

---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ChainConstructorsAndVariables/ChainConstructorsAndVariables/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainConstructorsAndVariables
+{
+    class Employee
+    {
+        // Constructor call chain that inherits properties from the original constructor
+        public Employee (string fname, string lname, int id) : this(fname, _Blank, lname, id) { }
+
+        // Original constructor 
+        public Employee (string fname, string mname, string lname, int id)
+        {
+            Firstname = fname;
+            MiddleName = mname;
+            LastName = lname;
+            Id = id;
+        }
+
+        public string Firstname { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        private const string _Blank = "";
+    }
+}
---
+++ The_Tech_Academy_C-Sharp_Projects/Basic_C-Sharp_Projects/ChainConstructorsAndVariables/ChainConstructorsAndVariables/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainConstructorsAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TechCo Co\nYou're a new employee");
+            Console.WriteLine("We need to collect some information from you.");
+            Console.WriteLine("Please input your first name:");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Do you have a middle name? Please input 'true' or 'false'.");
+            var hasMiddleName = Convert.ToBoolean(Console.ReadLine().ToLower());
+            string middleName = null;
+            if (hasMiddleName)
+            {
+                Console.WriteLine("Please input your middle name:");
+                middleName = Console.ReadLine();
+            }
+            Console.WriteLine("Please input your last name:");
+            var lastName = Console.ReadLine();
+            Console.WriteLine("Please input your desired employee id number:");
+            var idNumber = Convert.ToInt32(Console.ReadLine());
+
+            Employee employee = null;
+            if (hasMiddleName)
+            {
+                employee = new Employee(firstName, middleName, lastName, idNumber);
+                Console.WriteLine("Employee info:\n");
+                Console.WriteLine("{0}, {1}, {2}, {3}", employee.Firstname, employee.MiddleName, employee.LastName, employee.Id);
+                Console.WriteLine("Successful! Your info has been added to our employee database.");
+            }
+            else
+            {
+                employee = new Employee(firstName, lastName, idNumber);
+                Console.WriteLine("Employee info:\n");
+                Console.WriteLine("{0}, {1}, {2}", employee.Firstname, employee.LastName, employee.Id);
+                Console.WriteLine("Successful! Your info has been added to our employee database.");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
