b67a171552236fa95843bc9c8b446d39b2c6380a
Courtney Dart <dart.courtney1@gmail.com>
2020-07-24 16:25:31 -0700

    Create controller and views, update Enrollment class enum.

--- The_Tech_Academy_C-Sharp_Projects/.Net_Projects/ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ The_Tech_Academy_C-Sharp_Projects/.Net_Projects/ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace ContosoUniversity.Models
 {
+    // We use enum for our list of Grades, since the letter grades are non-changing.
+    // The enum must be placed outside of the Enrollment class to avoid ambiguity.
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
     public class Enrollment
     {
-        // We use enum for our list of Grades, since the letter grades are non-changing.
-        public enum Grade
-        {
-            A, B, C, D, F
-        }
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
         // The Grade type indicates the Grade property is nullable.
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         // These are navigation properties that hold other entities related to this entity.
