59be72ed5413d808f2a4eb88c9641282db1a314d
Courtney Dart <dart.courtney1@gmail.com>
2020-07-24 14:29:47 -0700

    Create entity classes.

---
+++ The_Tech_Academy_C-Sharp_Projects/.Net_Projects/ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Enrollment
+    {
+        // We use enum for our list of Grades, since the letter grades are non-changing.
+        public enum Grade
+        {
+            A, B, C, D, F
+        }
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        // The Grade type indicates the Grade property is nullable.
+        public Grade? Grade { get; set; }
+
+        // These are navigation properties that hold other entities related to this entity.
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
