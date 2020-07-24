57af66f0605fddd85e595d09675af620cbc0d230
Courtney Dart <dart.courtney1@gmail.com>
2020-07-24 10:52:19 -0700

    Update .txt file.

---
+++ The_Tech_Academy_C-Sharp_Projects/.Net_Projects/Scores/Scores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome Back {uName}. Today is {date}.";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Tech-Academy-Projects\The_Tech_Academy_C-Sharp_Projects\.Net_Projects\Scores\Scores\studentScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            double tScore = 0.0;
+
+            Console.WriteLine("\nStudent Scores: \n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " student scores. \tAverage score: "  + avgScore);
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
