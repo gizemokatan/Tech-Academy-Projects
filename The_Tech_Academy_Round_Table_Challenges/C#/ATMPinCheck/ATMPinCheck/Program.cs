3149a480be4c0be94afde94f2e7c41ee15773b5d
Courtney Dart <dart.courtney1@gmail.com>
2020-10-02 10:08:58 -0700

    Add code challenges from Sept 30.

---
+++ The_Tech_Academy_Round_Table_Challenges/C#/ATMPinCheck/ATMPinCheck/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ATMPinCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome, please enter a 4 or 6 digit pin number: ");
+            string userPin = Console.ReadLine();
+            Program program = new Program();
+            program.MyFunction(userPin);
+        }
+
+        public void MyFunction(string userPin)
+        {
+            bool pinValid = true;
+            int pinLen = userPin.Length;
+
+            if (userPin == "")
+            {
+                pinValid = false;
+            }
+            else if (userPin != "")
+            {
+                for (int i = 0; i < userPin.Length; i++)
+                {
+                    if (pinLen == 4 || pinLen == 6)
+                    {
+                        if (userPin[i] >= 'a' && userPin[i] <= 'z')
+                        {
+                            pinValid = false;
+                        }
+                    }
+                    else if (pinLen != 4 || pinLen != 6)
+                    {
+                        pinValid = false;
+                    }
+                    else
+                    {
+                        pinValid = true;
+                    }
+                }
+            }
+            if (pinValid == true)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
---
+++ The_Tech_Academy_Round_Table_Challenges/C#/repeatStringChar/repeatStringChar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace repeatStringChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a simple phrase to have each character doubled: ");
+            string txt = Console.ReadLine();
+            Program program = new Program();
+            program.DoubleChar4(txt);
+        }
+
+        public void DoubleChar(string txt)
+        {
+            string finaltxt = "";
+            for (int i = 0; i < txt.Length; i++)
+            {
+                finaltxt = finaltxt + txt[i] + txt[i];
+            }
+            Console.WriteLine("Your phrase: " + finaltxt);
+        }
+
+        public void DoubleChar2(string txt)
+        {
+            List<char> charList = new List<char>();
+            for (int i = 0; i < txt.Length; i++)
+            {
+                charList.Add(txt[i]);
+                charList.Add(txt[i]);
+            }
+            string str = new string(charList.ToArray());
+            Console.WriteLine(str);
+        }
+
+        public void DoubleChar3(string txt)
+        {
+            StringBuilder strList = new StringBuilder();
+            for (int i = 0; i < txt.Length; i++)
+            {
+                strList.Append(txt[i]);
+                strList.Append(txt[i]);
+            }
+            Console.WriteLine(strList.ToString());
+        }
+
+        public void DoubleChar4(string txt)
+        {
+            StringBuilder strList = new StringBuilder();
+            for (int i = 0; i < txt.Length; i++)
+            {
+                string str = new string(txt[i], 2);
+                strList.Append(str);
+            }
+            Console.WriteLine(strList.ToString());
+        }
+
+    }
+}
